57db172be7b3cdc4a049a15db437408630f6ac99
kanboo <wang.kanboo@gmail.com>
2017-12-13 00:03:46 +0800

    新增 public 資料夾

---
+++ public/js/all.js
@@ -0,0 +1,9 @@
+'use strict';
+
+$(document).ready(function () {
+    $('.showmenu').click(function (e) {
+        e.preventDefault();
+        $('.navbar').toggleClass('memushow');
+    });
+});
+//# sourceMappingURL=all.js.map
